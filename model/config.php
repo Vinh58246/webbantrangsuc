c4bb39f46cf476648027b73b07ae1240986432da
vinh Nguyen <135817122+Vinh58246@users.noreply.github.com>
2024-12-27 01:18:09 +0700

    Update config.php

--- model/config.php
+++ model/config.php
@@ -3,18 +3,18 @@
 const host_email = "smtp.gmail.com";
 const port_eamil = 465;
 const name_email = "Super Wow";
-const address_email = "nguyenphuocvinh051204@gmail.com";
-const pass_email = "jfhlxforuadrwwkx";
+const address_email = "nhập email của bạn";
+const pass_email = "nhập mật khẩu email của bạn";
 
 // cấu hình database
 const servername = "localhost";
-const user_name = "phuocvinh";
-const user_password = "phuocvinh123";
-const db_name = "sw";
+const user_name = "root";
+const user_password = "";
+const db_name = "kta";
 
 // cấu hình thanh toán momo 
-const path_pay = "https://web1.orggenji.click/?mod=page&act=messageSuccessful";
-// const path_pay = "http://localhost:3000/index.php?mod=page&act=messageSuccessful";
+// const path_pay = "https://web1.orggenji.click/?mod=page&act=messageSuccessful";
+const path_pay = "http://localhost:3000/index.php?mod=page&act=messageSuccessful";
 const partner_Code = "MOMOBKUN20180529";
 const access_Key = "klm05TvNBzhg7h7j";
 const secret_Key = "at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa";
@@ -22,5 +22,5 @@ const secret_Key = "at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa";
 // cấu hình đăng nhập google
 const clientID = '756384640485-los90hkrpj2c8f7qcj6pdn6su6an9isp.apps.googleusercontent.com';
 const clientSecret = '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';
-const redirectUri = 'https://web1.orggenji.click/?mod=page&act=dangnhap';
-// const redirectUri = 'http://localhost:3000/index.php?mod=page&act=dangnhap';
+// const redirectUri = 'https://web1.orggenji.click/?mod=page&act=dangnhap';
+const redirectUri = 'http://localhost:3000/index.php?mod=page&act=dangnhap';
